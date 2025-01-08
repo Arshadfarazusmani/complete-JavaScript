5f80a147282fa488ef5afb38b097ad738e8f7add
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-01-08 22:13:55 +0530

    add variable.js  file to codebase

---
+++ one/variable.js
@@ -0,0 +1,22 @@
+//VARIABLE : A variable is a named storage for data . just like containers, use to store values
+
+// "let" : keyword use to create variable . -> values can be change after defination 
+
+let name = "Faraz ";
+let age = 40;
+
+// CONSTANTS : The Unchanged variables 
+// const keyword is use  -> No updation allowed 
+
+const Name ="Arshad "
+const Age = 30;
+
+// both let and const have block scope :-> 
+
+if(true){
+    let userName = "its_Mohtish"
+}
+
+// console.log(userName);  // can not access vriable outside the scope 
+
+//  * GIVE MEANINGFUL VARIABLE NAME 
