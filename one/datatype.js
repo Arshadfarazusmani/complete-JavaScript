263d14a16fc99bab2f33fcf595c0e6338bb5cc20
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-01-08 22:40:31 +0530

    add datatype.js  file to codebase

---
+++ one/datatype.js
@@ -0,0 +1,38 @@
+// JAVASCRIPT is dynamically typed language : no need to specify the datatype while assignning the value to variables .
+
+//  DYNAMICALLY TYPED LANGUAGES : where interprater assign the type at run time based on the value given . 
+
+// DATA TYPE : type of data a variable holds
+
+// In JS Data-types are classified into two catogories 
+        //  -> premitive 
+        //  -> Non-Premitive (object)
+
+//  Premitive : Number , String , Boolean , null , undefined , null , symbol 
+
+const name ="Faraz " ;// string -> sequence of charecters.
+
+// Number 
+let  age = 21; // Number -> int, decimal , +Infinity , -Infinity, NaN
+
+console.log(0/1); // Infinity 
+console.log(-0/1); // -Infinity
+console.log(-"a"/0); // NaN
+
+// Boolean -> True or False 
+const Is_Logged_In=true;
+const Is_admin=false;
+
+
+// null: -> stand alone value 
+// undefined -> not defined yet 
+
+// BigInt -> for large value 
+
+// Symbol -> associated to objects *look later -> Use to creste unique identifiers for objects 
+
+
+// Non premitive : Object Array functions 
+// Later 
+
+
